979fc920bcfeded3834ef0bf1bd5fe2aa5d78b1a
Pratish <pratishrai724@gmail.com>
2020-06-20 20:06:46 +0530

    users

--- Swype/urls.py
+++ Swype/urls.py
@@ -14,13 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from profiles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-urlpatterns = [
-    path('',views.index, name="index")
+    path('', views.index, name="index"),
+    path('accounts/', include('users.urls'))
 ]
