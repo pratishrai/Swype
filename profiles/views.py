26615861ea5182370540f705a36f19356bc61e38
sayamss <sawaisayam@gmail.com>
2020-06-20 19:31:21 +0530

    Added UrlPattern for Profiles, Added Base template

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+# Index Function
+def index(request):
+    return render(request,'index.html')
